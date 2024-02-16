8f08d93a171ea21af04fba3a755546c0c89a6cab
rdeager14 <rdeager@miners.utep.edu>
2024-02-15 22:00:26 -0700

    Update code_4.py

--- code_4.py
+++ code_4.py
@@ -6,7 +6,7 @@ big_mac_file = './big-mac-full-index.csv'
 def get_big_mac_price_by_year(year, country_code):
 
     data = pd.read_csv(big_mac_file)
-    filtered_data = data[(data['date'] == year) & (data['iso_a3'] == country_code.upper())]
+    filtered_data = data[(data['date'].str.startswith(str(year))) & (data['iso_a3'] == country_code.upper())]
     mean_price = filtered_data['dollar_price'].mean()
     return round(mean_price, 2)
 
@@ -18,13 +18,13 @@ def get_big_mac_price_by_country(country_code):
 
 def get_the_cheapest_big_mac_price_by_year(year):
     data = pd.read_csv(big_mac_file)
-    filtered_data = data[data['date'] == year]
+    filtered_data = data[data['date'].str.startswith(str(year))]
     min_price = filtered_data['dollar_price'].min()
     return round(min_price, 2)
 
 def get_the_most_expensive_big_mac_price_by_year(year):
     data = pd.read_csv(big_mac_file)
-    filtered_data = data[data['date'] == year]
+    filtered_data = data[data['date'].str.startswith(str(year))]
     max_price = filtered_data['dollar_price'].max()
     return round(max_price, 2)
 
